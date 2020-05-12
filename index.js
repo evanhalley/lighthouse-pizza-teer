95a88e8187160e07a745de4f100356f90d5bbb34
Evan Halley <evan.halley@gmail.com>
2020-05-12 17:31:26 -0400

    using a different pizza vendor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
 const config = require('lighthouse/lighthouse-core/config/lr-desktop-config.js');
@@ -7,49 +8,56 @@ const reportGenerator = require('lighthouse/lighthouse-core/report/report-genera
 
 require('dotenv').config();
 
-const PIZZA_PROFILE_URL = 'https://www.dominos.com/en/pages/customer/#!/customer/profile/';
+const PIZZA_PROFILE_URL = 'https://salviospizza.hungerrush.com/Account/Manage';
 
 (async() => {
-    let { browser, page } = await navigateToPizzaProfile()
-    
-    console.log('Running lighthouse...');
-    const { report } = await lighthouse(page.url(), {
-        port: (new URL(browser.wsEndpoint())).port,
-        output: 'json',
-        logLevel: 'info',
-        disableDeviceEmulation: true,
-        chromeFlags: ['--disable-mobile-emulation']
-    }, config);
-    const json = reportGenerator.generateReport(report.lhr, 'json');
-    console.log(`Lighthouse scores: ${json}`);
+    let browser = null;
+    let page = null;
 
-    await page.close();
-    await browser.close();
+    try {
+        browser = await navigateToPizzaProfile();
+        page = (await browser.pages())[0];
+        console.log('Running lighthouse...');
+        const report = await lighthouse(page.url(), {
+            port: (new URL(browser.wsEndpoint())).port,
+            output: 'json',
+            logLevel: 'info',
+            disableDeviceEmulation: true,
+            chromeFlags: ['--disable-mobile-emulation']
+        }, config);
+        const json = reportGenerator.generateReport(report.lhr, 'json');
+        const html = reportGenerator.generateReport(report.lhr, 'html');
+        console.log(`Lighthouse scores: ${report.lhr.score}`);
+
+        console.log('Writing results...');
+        fs.writeFileSync('report.json', json);
+        fs.writeFileSync('report.html', html);
+        console.log('Done!');
+    } catch (error) {
+        console.error('Error!', error);
+    } finally {
+        await page.close();
+        await browser.close();
+    }
 })();
 
 async function navigateToPizzaProfile() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
 
     console.log('Navigating to Pizza Profile...');
     const page = (await browser.pages())[0];
     await page.goto(PIZZA_PROFILE_URL, { waitUntil: 'networkidle0' });
 
-    console.log('Starting login...');
-    await Promise.all([
-        await page.click('#js-rewards > div > section.section--full.section--blue > div > div.profile-links > p:nth-child(2) > button'),
-        await page.waitForSelector('#Email')
-    ]);
-
-    console.log('Entering username and password...');
-    await page.type('#Email', process.env.DOMINOS_USERNAME);
-    await page.type('#Password', process.env.DOMINOS_PASSWORD);
+    console.log('Starting login, entering username and password...');
+    await page.type('#UserName', process.env.USERNAME);
+    await page.type('#Password', process.env.PASSWORD);
 
     console.log('Logging in....');
     await Promise.all([
         page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        page.click('#pizzaProfileLoginOverlay > form > div.grid > div > div.js-anonymous > div:nth-child(3) > div.grid.loginButtonsContainer > div:nth-child(2) > button'),
+        page.click('#btnLogin'),
     ]);
 
     console.log('Pizza profile unlocked!');
-    return { browser: browser, page: page };
+    return browser;
 }
